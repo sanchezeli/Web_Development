7f17d6a6c0899a98c6616e4b46c16127740df90d
sanchezeli <elias.sanchez2@g.austincc.edu>
2021-07-17 12:12:59 -0500

    testing

---
+++ script.js
@@ -0,0 +1,7 @@
+function name(){var first= prompt("Please enter your name.");
+var second = prompt ("What is your favorite snack?");
+var third = prompt("What is your favorite drink?");
+return "Hi there "+
+first +" , it seems that you enjoy eating " + second +" and drinking "+ third;}
+document.write(name())
+
